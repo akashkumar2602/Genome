bfdbf95f12d06daf168b586feaa736d333caa7c3
Akash <akash.saw@capgemini.com>
2025-02-18 23:11:32 +0100

    fixed build issue

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,28 @@
-import type { NextConfig } from "next";
+const nextConfig = {
+  experimental: {
+    optimizeCss: true, // Enables CSS optimizations
+    optimizeFonts: true, // Enables font optimizations
+  },
+  webpack(config: {
+    optimization: {
+      usedExports: boolean;
+      splitChunks: {
+        chunks: string;
+        minSize: number; // Set minimum size for chunking (50 KB)
+        maxSize: number;
+      };
+    };
+  }) {
+    // Enable tree shaking to remove unused code
+    config.optimization.usedExports = true;
+    config.optimization.splitChunks = {
+      chunks: "all",
+      minSize: 50 * 1024, // Set minimum size for chunking (50 KB)
+      maxSize: 250 * 1024, // Set max size for chunking (250 KB)
+    };
 
-const nextConfig: NextConfig = {
-  /* config options here */
+    return config;
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
